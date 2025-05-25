14fab10a15a5d084dbd0cfea9abfd752a88a1fd8
Amine Saoudi <saoudiamine8@gmail.com>
2025-05-24 20:47:45 -0400

    webhook test

---
+++ app.js
@@ -0,0 +1,21 @@
+// server.js
+import express from 'express';
+import { exec } from 'child_process';
+
+const app = express();
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    // exec('bash /path/to/deploy.sh', (err, stdout, stderr) => {
+    //     if (err) {
+    //         console.error('Deploy failed:', stderr);
+    //         return res.sendStatus(500);
+    //     }
+    //     console.log('Deploy output:', stdout);
+    //     res.sendStatus(200);
+    // });
+
+    console.log("Push detected")
+});
+
+app.listen(3000, () => console.log('Listening for webhook on port 3000'));
